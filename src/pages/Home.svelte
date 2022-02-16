59474b77cbad899afe0e4666f4f90a15a32526c5
kenta640 <kenta0803d@gmail.com>
2022-02-16 16:49:06 +1300

    add

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,6 +1,10 @@
 <script></script>
 <main>
-    This Application is a simple Social Media application.
+    This Application is a simple Social Media application built by Svelte, TailwindCSS, GraphQL, MongoDB.
 
+    <br/>
+    
     Register User from the register page. False email can also be used to register.
+
+
 </main>
